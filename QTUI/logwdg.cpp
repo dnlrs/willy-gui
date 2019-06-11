e6f2b8573ede4695cdd49ce126a3ad90541ab1d1
Daniel Rusu <rusudaniel39@gmail.com>
2019-06-11 20:50:31 +0100

    uncommented process start

--- QTUI/logwdg.cpp
+++ QTUI/logwdg.cpp
@@ -15,12 +15,12 @@ logwdg::logwdg(QWidget *parent) : QMainWindow(parent)
     HLayout->addWidget(edit, 0,0);
     wdg->setLayout (HLayout);
 
-    //this->server = new QProcess(this);
-    //this->server->setWorkingDirectory("..\\server");
+    this->server = new QProcess(this);
+    this->server->setWorkingDirectory("..\\server");
     //out<< server->pid() << endl;
-    //connect(this->server, SIGNAL(readyReadStandardOutput()), this, SLOT(processOutput()));  // connect process signals with your code
-    //connect(this->server, SIGNAL(readyReadStandardError()), this, SLOT(processOutput()));  // same here
-    //server->start("..\\server\\wifi_watchdog_analyzer.exe");
+    connect(this->server, SIGNAL(readyReadStandardOutput()), this, SLOT(processOutput()));  // connect process signals with your code
+    connect(this->server, SIGNAL(readyReadStandardError()), this, SLOT(processOutput()));  // same here
+    server->start("..\\server\\wifi_watchdog_analyzer.exe");
     //out<< server->pid() << endl;
     //out << server->state() << endl;
 }
